fbff16fe6cf1a5afd490505f69db247167b55661
Santiago Bernaldo <sochoa.bernaldo@atsistemas.com>
2023-12-26 14:25:18 +0100

    First commit, and configuring tests

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Santi Bernaldo on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
