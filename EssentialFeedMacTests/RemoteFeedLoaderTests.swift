d8a046c8505c9a3b333279c786b467d8a28ad11c
Santiago Bernaldo <sochoa.bernaldo@atsistemas.com>
2023-12-29 16:29:31 +0100

    Asserting the number of times method load was invoked on RemoteFeedLoader

--- EssentialFeedMacTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedMacTests/RemoteFeedLoaderTests.swift
@@ -10,20 +10,34 @@ import EssentialFeed
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
-    func test_init_DoesNotRequestDataFromURL() {                
+    func test_init_DoesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
                 
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLS.isEmpty)
     }
     
-    func test_loadRequestDataFromURL() {
+    func test_load_RequestDataFromURL() {
         let url = URL(string: "http://agivenurl.com")!
                 
         let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
-        XCTAssertEqual(client.requestedURL, url)
+        // When asserting objects collaborating, is not enough to test the values passed, but we need to ask how many times was the method invoked
+        XCTAssertEqual(client.requestedURLS, [url])
+    }
+    
+    // We test that 'load' is only called once from the RemoteFeedLoader, using the client. Cause this code can gets duplicated during merge or other situations, and we are bound to avoid that.
+    func test_loadTwice_RequestDataFromURL() {
+        let url = URL(string: "http://agivenurl.com")!
+                
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        // When asserting objects collaborating, is not enough to test the values passed, but we need to ask how many times was the method invoked
+        XCTAssertEqual(client.requestedURLS, [url, url])
     }
     
     private func makeSUT(url: URL = URL(string: "http://agivenurl.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
@@ -36,10 +50,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
                       
                       
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        var requestedURLS = [URL]()
             
         func load(url: URL) {
-            requestedURL = url
+            requestedURLS.append(url)
         }
     }
 }
