198c026ac84552d1f6602e95d926a110a9e76c33
Santiago Bernaldo <quirosdeveloper@gmail.com>
2024-03-02 17:44:22 +0100

    Protocol-based mocking without overriding Foundation dependant APIs methods

--- CI/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ CI/EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -6,17 +6,31 @@
 //
 
 import XCTest
+import EssentialFeed
+
+protocol HTTPURLSession {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPURLSessionDataTask
+}
+
+protocol HTTPURLSessionDataTask {
+    func resume()
+}
 
 class URLSessionHTTPClient {
     
-    let session: URLSession
+    let session: HTTPURLSession
     
-    init(session: URLSession) {
+    init(session: HTTPURLSession) {
         self.session = session
     }
     
-    func load(url: URL) {
-        session.dataTask(with: url) { _, _, _ in }
+    func load(url: URL, completion: @escaping (HTTPClientResult) -> ()) {
+        session.dataTask(with: url) { _, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+        }.resume()
     }
     
 }
@@ -32,39 +46,77 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         let sut = URLSessionHTTPClient(session: session)
         
-        sut.load(url: url)
+        sut.load(url: url) { _ in }
         
         // ReceivedURLS is a test detail
         XCTAssertEqual(task.resumeCallCount, 1)
     }
     
-    // MARK: - Helpers
-    private class URLSessionSpy: URLSession {
+    func test_getFromURL_failsOnRequestError() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let error = NSError(domain: "Any error", code: 0)
         
-        private var stubs = [URL: URLSessionDataTask]()
+        session.stub(url: url, error: error)
         
-        func stub(url: URL, dataTask: URLSessionDataTask) {
-            stubs[url] = dataTask
+        let sut = URLSessionHTTPClient(session: session)
+        
+        let exp = expectation(description: "Wait for completion")
+        
+        sut.load(url: url) { result in
+            switch result {
+            case let .failure(receivedError as NSError):
+                XCTAssertEqual(receivedError, error)
+                
+            default:
+                XCTFail("Expected failure with \(error) got \(result) instead")
+            }
+            
+            exp.fulfill()
         }
         
-        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            return stubs[url] ?? FakeURLSessionDataTask()
+        // ReceivedURLS is a test detail
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    private class URLSessionSpy: HTTPURLSession {
+        
+        private var stubs = [URL: Stub]()
+        
+        struct Stub {
+            let task: HTTPURLSessionDataTask
+            let error: Error?
+        }
+        
+        func stub(url: URL, dataTask: HTTPURLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
+            stubs[url] = Stub(task: dataTask, error: error)
+        }
+        
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPURLSessionDataTask {
+            guard let stub = stubs[url] else {
+                fatalError("We couldn't find a stub with the given url \(url)")
+            }
+            
+            completionHandler(nil, nil, stub.error)
+            
+            return stub.task
         }
     }
     
     // Mocking an URLSessionDataTask which carries lots of methods that we're not overriding, and these methods can be interoperating between them, so we're doing a big assumption here and risking
     
     // We don't own this classes, so this is a big risk
-    private class FakeURLSessionDataTask: URLSessionDataTask {
-        override func resume() {
+    private class FakeURLSessionDataTask: HTTPURLSessionDataTask {
+        func resume() {
             
         }
     }
     
-    private class URLSessionDataTaskSpy: URLSessionDataTask {
+    private class URLSessionDataTaskSpy: HTTPURLSessionDataTask {
         var resumeCallCount = 0
         
-        override func resume() {
+        func resume() {
             resumeCallCount += 1
         }
     }
