02c07136a2e7ca89cef942082746353fb15940f3
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-04-02 16:58:30 +0200

    Add RemoteFeedItem Data Transfer Representation to decouple from FeedItem data module

--- CI/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ CI/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -41,14 +41,28 @@ public final class RemoteFeedLoader: FeedLoader {
             
             switch result {
             case .success(let data, let response):
-                completion(FeedItemMapper.map(data, response))
+                completion(RemoteFeedLoader.map(data, response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
         }
     }
+    
+    private static func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemMapper.map(data, response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
 }
 
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem] {
+        return map { FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image) }
+    }
+}
 
 
 
--- CI/EssentialFeed/EssentialFeed/Feed Feature/FeedItemMapper.swift
+++ CI/EssentialFeed/EssentialFeed/Feed Feature/FeedItemMapper.swift
@@ -5,38 +5,26 @@
 //  Created by Santi Bernaldo on 27/2/24.
 //
 
+// Internal representation of the FeedItem for the API Module
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal var description: String?
+    internal let location: String?
+    internal let image: URL
+}
+
 internal final class FeedItemMapper {
     private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedItem] {
-            return items.map { $0.item }
-        }
+        let items: [RemoteFeedItem]
     }
 
-    // Internal representation of the FeedItem for the API Module
-    private struct Item: Decodable {
-        let id: UUID
-        var description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            return FeedItem(
-                id: id,
-                description: description,
-                location: location,
-                imageURL: image)
-        }
-    }
-    
     private static var OK_200: Int { 200 }
     
-    static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
         
-        return .success(root.feed)
+        return root.items
     }
 }
