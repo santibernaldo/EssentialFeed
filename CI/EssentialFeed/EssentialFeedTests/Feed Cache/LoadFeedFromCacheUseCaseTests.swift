552bf716d13023dd5be47a8ba53c0f1e6d1fb245
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-04-02 18:34:45 +0200

    TEST-DRIVEN ⛔️ delivering no images on Empty Cache. We now need the 'SUT' to return a result with images and an error

--- CI/EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ CI/EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -78,6 +78,24 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
+    
+    func test_load_delivesNoImagesOnEmptyCache() {
+        let (store, sut) = makeSUT()
+        let retrievalError = anyNSError()
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedImages: [FeedImage]?
+        sut.load { images in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeRetrieval(with: retrievalError)
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedError as NSError?, retrievalError)
+    }
 
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init ,file: StaticString = #filePath,
