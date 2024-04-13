d83a95aedf9a4d51e4d9f001731b3d2348f780e6
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-04-13 17:02:36 +0200

    Proved the CodableFeedStore side-effects run serially

--- CI/EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ CI/EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -152,6 +152,35 @@ final class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .emptyCache)
     }
     
+    // Orders matters, so that's what we measure here. The order of being executed these operations
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        
+        var completedOperationsInOrder = [XCTestExpectation]()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(makeUniqueFeed().local, timestamp: Date(), completion: { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        })
+                   
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCacheFeed(completion: { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        })
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(makeUniqueFeed().local, timestamp: Date(), completion: { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        })
+     
+        waitForExpectations(timeout: 5.0)
+        
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3])
+    }
+                                       
     // - MARK: Helpers
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
         let sut = CodableFeedStore(storeURL: storeURL ?? testsSpecificStoreURL())
