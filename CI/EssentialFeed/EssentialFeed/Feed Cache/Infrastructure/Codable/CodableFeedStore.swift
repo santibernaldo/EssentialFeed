979dda70c807385fd08db3a65f193f455abd267e
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-04-16 19:07:45 +0200

    Extract CoreData managed model classes into separate files

--- CI/EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/Codable/CodableFeedStore.swift
+++ CI/EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/Codable/CodableFeedStore.swift
@@ -98,16 +98,3 @@ public class CodableFeedStore: FeedStore {
     }
 }
 
-
-private class ManagedCache: NSManagedObject {
-    @NSManaged var timestamp: Date
-    @NSManaged var feed: NSOrderedSet
-}
-
-private class ManagedFeedImage: NSManagedObject {
-    @NSManaged var id: UUID
-    @NSManaged var imageDescription: String?
-    @NSManaged var location: String?
-    @NSManaged var url: URL
-    @NSManaged var cache: ManagedCache
-}
