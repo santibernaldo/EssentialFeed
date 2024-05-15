f4d3118d13cfe1a7c731f883cdf797aa1348db6b
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-05-15 11:17:45 +0200

    Option to retry on image download error

--- CI/EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ CI/EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -75,16 +75,24 @@ public final class FeedViewController: UITableViewController {
         cell.feedImageContainer.startShimmering()
         cell.feedImageRetryButton.isHidden = true
         
-        tasks[indexPath] = feedImageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
-            let data = try? result.get()
-            let image = data.map(UIImage.init) ?? nil
-            cell?.feedImageView.image = image
-            cell?.feedImageContainer.stopShimmering()
-            cell?.feedImageRetryButton.isHidden = (image != nil)
+        let loadImage = { [weak self, weak cell] in
+            guard let self = self else { return }
+            
+            self.tasks[indexPath] = self.feedImageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.feedImageContainer.stopShimmering()
+            }
         }
+        
+        cell.onRetry = loadImage
+        loadImage()
+        
         return cell
     }
-    
+        
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         tasks[indexPath]?.cancel()
         tasks[indexPath] = nil
