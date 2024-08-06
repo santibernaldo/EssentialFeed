f593b3bc1865a02e467139e3b1ca5145e653f9cd
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-06 17:14:05 +0200

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness - We can now run UI tests without internet connection.

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -7,16 +7,15 @@
 
 import XCTest
 
-final class EssentialAppUIAcceptanceTests: XCTestCase {
-
+class EssentialAppUIAcceptanceTests: XCTestCase {
+    
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
-        app.launchArguments = ["reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
         app.launch()
         
-        // Identifier added on the 'Identifier' field of the Custom Class of the Inspector's View
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
@@ -24,18 +23,15 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
-        // We reset here the state, so the next time the app launches on this test, we can use the cache saved
-        onlineApp.launchArguments = ["reset"]
+        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
         onlineApp.launch()
-        
-        // After we've run the app for the first time, and saved on to the cache, we use the 'launchArguments' identifiers, to identify that we are running the app with the Cache Saved. So we can fake the Offline connection, and test that we are showing
-        // the cells without Connectivity
+
         let offlineApp = XCUIApplication()
         offlineApp.launchArguments = ["-connectivity", "offline"]
         offlineApp.launch()
-        
+
         let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cachedFeedCells.count, 22)
+        XCTAssertEqual(cachedFeedCells.count, 2)
         
         let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstCachedImage.exists)
@@ -43,12 +39,11 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
         let app = XCUIApplication()
-        
-        // We reset of the test passing another flag. Because some of the other test, left artifacts (cache), we have to reset the state.
-        app.launchArguments = ["reset", "-connectivity", "offline"]
+        app.launchArguments = ["-reset", "-connectivity", "offline"]
         app.launch()
-        
+
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
         XCTAssertEqual(feedCells.count, 0)
     }
+    
 }
