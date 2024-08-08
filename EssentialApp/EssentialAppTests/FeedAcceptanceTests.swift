8dd2ebe996c961e4ef38759c853933655c366d42
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-08 11:29:44 +0200

    Fixed scale for images to 1 which prevent a different scale assigned when converted to Data

---
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -0,0 +1,8 @@
+//
+//  FeedAcceptanceTests.swift
+//  EssentialAppTests
+//
+//  Created by Santiago Ochoa Bernaldo de Quiros on 7/8/24.
+//
+
+import Foundation
--- EssentialApp/EssentialAppTests/Helpers/UIImage+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/UIImage+TestHelpers.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 extension UIImage {
+    //On iOS 16.1, comparing images generated with the UIImage.make(color:) helper started to fail due to differences in the image scale when converting it to pngData. To prevent this issue, you need to update the UIImage.make(color:) helper implementation to set a fixed scale of 1.
+    
     static func make(withColor color: UIColor) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
+        
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        
+        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(rect)
+        }
     }
 }
