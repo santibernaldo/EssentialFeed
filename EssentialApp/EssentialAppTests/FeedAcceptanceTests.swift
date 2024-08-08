42e694dfa66fa7d6b483e98f9ddc5583d9e448c9
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-08 18:36:35 +0200

    Displays cached feed on launch when customer has no connectivity

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -5,4 +5,140 @@
 //  Created by Santiago Ochoa Bernaldo de Quiros on 7/8/24.
 //
 
-import Foundation
+
+import XCTest
+import EssentialFeed
+import EssentialFeediOS
+@testable import EssentialApp
+
+// Tests for top level UI Acceptance, without using the UI Xcode framework tests which are really heavy in terms of time
+class FeedAcceptanceTests: XCTestCase {
+
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let feed = launch(httpClient: .online(response), store: .empty)
+
+        XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
+        XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData())
+        XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData())
+    }
+    
+    func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+        let sharedStore = InMemoryFeedStore.empty
+        let onlineFeed = launch(httpClient: .online(response), store: sharedStore)
+        onlineFeed.simulateFeedImageViewVisible(at: 0)
+        onlineFeed.simulateFeedImageViewVisible(at: 1)
+        
+        let offlineFeed = launch(httpClient: .offline, store: sharedStore)
+        
+        XCTAssertEqual(offlineFeed.numberOfRenderedFeedImageViews(), 2)
+        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 0), makeImageData())
+        XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 1), makeImageData())
+    }
+    
+    func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
+
+    }
+
+    // MARK: - Helpers
+
+    private func launch(
+        httpClient: HTTPClientStub = .offline,
+        store: InMemoryFeedStore = .empty
+    ) -> FeedViewController {
+        let sut = SceneDelegate(httpClient: httpClient, store: store)
+        sut.window = UIWindow()
+        sut.configureWindow()
+
+        let nav = sut.window?.rootViewController as? UINavigationController
+        let vc = nav?.topViewController as! FeedViewController
+        /*
+         The refresh method will be called on viewIsAppearing to start the request. But the test never calls viewIsAppearing, so the refresh method is never called, and the test fails because it never start to load the data. You need to trigger viewIsAppearing in the test to hit the refresh method and start the request. You can do it in the launch method:
+         */
+        vc.simulateAppearance()
+        return vc
+    }
+
+    private class HTTPClientStub: HTTPClient {
+        private class Task: HTTPClientTask {
+            func cancel() {}
+        }
+
+        private let stub: (URL) -> HTTPClient.Result
+
+        init(stub: @escaping (URL) -> HTTPClient.Result) {
+            self.stub = stub
+        }
+
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+            completion(stub(url))
+            return Task()
+        }
+
+        static var offline: HTTPClientStub {
+            HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
+        }
+
+        static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> HTTPClientStub {
+            HTTPClientStub { url in .success(stub(url)) }
+        }
+    }
+
+    private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
+        private var feedCache: CachedFeed?
+        private var feedImageDataCache: [URL: Data] = [:]
+
+        func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+            feedCache = nil
+            completion(.success(()))
+        }
+
+        func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+            feedCache = CachedFeed(feed: feed, timestamp: timestamp)
+            completion(.success(()))
+        }
+
+        func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+            completion(.success(feedCache))
+        }
+
+        func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+            feedImageDataCache[url] = data
+            completion(.success(()))
+        }
+
+        func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+            completion(.success(feedImageDataCache[url]))
+        }
+
+        static var empty: InMemoryFeedStore {
+            InMemoryFeedStore()
+        }
+    }
+
+    private func response(for url: URL) -> (Data, HTTPURLResponse) {
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        return (makeData(for: url), response)
+    }
+
+    private func makeData(for url: URL) -> Data {
+        switch url.absoluteString {
+        case "http://image.com":
+            return makeImageData()
+
+        default:
+            return makeFeedData()
+        }
+    }
+
+    private func makeImageData() -> Data {
+        return UIImage.make(withColor: .red).pngData()!
+    }
+
+    private func makeFeedData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            ["id": UUID().uuidString, "image": "http://image.com"],
+            ["id": UUID().uuidString, "image": "http://image.com"]
+        ]])
+    }
+
+}
