55eb763537129dd4b1d04f074317890fb36b23ab
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-11 12:57:24 +0200

    Improve a test name

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -11,7 +11,7 @@ import EssentialFeediOS
 
 class SceneDelegateTests: XCTestCase {
 
-    func test_sceneWillConnectToSession_configuresRootViewController() {
+    func test_configureWindow_configuresRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
 
