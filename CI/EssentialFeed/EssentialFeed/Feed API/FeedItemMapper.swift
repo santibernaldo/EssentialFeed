cdf921e137a7ac17c9305feb84c84dcbdd0b6751
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-24 18:08:39 +0200

    Move FeedImage mapping to the FeedItemsMapper

---
+++ CI/EssentialFeed/EssentialFeed/Feed API/FeedItemMapper.swift
@@ -0,0 +1,32 @@
+//
+//  FeedItemMapper.swift
+//  EssentialFeed
+//
+//  Created by Santi Bernaldo on 27/2/24.
+//
+
+final class FeedItemMapper {
+    private struct Root: Decodable {
+        private let items: [RemoteFeedItem]
+        
+        private struct RemoteFeedItem: Decodable {
+            let id: UUID
+            let description: String?
+            let location: String?
+            let image: URL
+        }
+        
+        var images: [FeedImage] {
+            items.map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
+        }
+    }
+
+
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
+            guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+                throw RemoteFeedLoader.Error.invalidData
+            }
+
+            return root.images
+        }
+}
--- CI/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ CI/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -50,19 +50,14 @@ public final class RemoteFeedLoader: FeedLoader {
     
     private static func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
         do {
-            let items = try FeedItemMapper.map(data, response)
-            return .success(items.toModels())
+            let items = try FeedItemMapper.map(data, from: response)
+            return .success(items)
         } catch {
             return .failure(error)
         }
     }
 }
 
-private extension Array where Element == RemoteFeedItem {
-    func toModels() -> [FeedImage] {
-        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
-    }
-}
 
 
 
