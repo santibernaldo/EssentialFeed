b1260cfab1b155f20ffc86d26c92a7793ba50c51
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-26 11:54:48 +0200

    Move tests to Shared scope

--- CI/EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ CI/EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -81,13 +81,3 @@ final class FeedItemsMapperTests: XCTestCase {
     
 }
 
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-    }
-}
-
-func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-    let json = ["items": items]
-    return try! JSONSerialization.data(withJSONObject: json)
-}
