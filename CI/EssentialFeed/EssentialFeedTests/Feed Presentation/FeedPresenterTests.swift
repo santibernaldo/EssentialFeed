a2d457819495345bb0ecd00e31900081d77d51d7
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-26 18:19:24 +0200

    Add FeedPresenter map

--- CI/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ CI/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -82,7 +82,7 @@ class FeedPresenterTests: XCTestCase {
         
         private(set) var messages = Set<Message>()
         
-        func display(_ viewModel: FeedErrorViewModel) {
+        func display(_ viewModel: ResourceErrorViewModel) {
             messages.insert(.display(errorMessage: viewModel.message))
         }
         
