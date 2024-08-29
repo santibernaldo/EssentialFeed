054a8d00e5e4b6da23703c06ecf7d52a50f5f6eb
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-08-29 09:47:33 +0200

    Extract Snapshot method tests into a Shared Helper class. And added test onto ListSnapshotTests

---
+++ CI/EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -0,0 +1,145 @@
+//
+//  FeedSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Santiago Ochoa Bernaldo de Quiros on 9/8/24.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+import EssentialFeed
+
+// First, we call the 'record' on every test to check the image created is the one we want to use in the future to compare it every time we run the tests. And then, we use the 'assert' to compare it with the previous snapshot saved
+
+/*
+ Some of the traits of snapshot testing are:
+
+ It allows you to easily review visuals on pull requests as you can store the snapshot artifacts in git.
+ They reveal visual bugs that other testing strategies won’t pick up easily (e.g., odd rendering in specific localizations).
+ They improve the collaboration with the design team as you can actually easily present them with visuals aids.
+ They allow you to automate the capturing of screenshots (e.g., for App Store Connect).
+ They offer a significantly more performant alternative for automating testing visuals than manual or UI testing.
+ Moreover, you can commit the snapshot artifacts to your git repository. Thus, everyone in the team will have access to them.
+
+
+ */
+
+class FeedSnapshotTests: XCTestCase {
+    
+    func test_emptyFeed() {
+        let sut = makeSUT()
+        
+        sut.display(emptyFeed())
+
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
+    }
+    
+    func test_feedWithContent() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithContent())
+
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
+    }
+    
+    
+    
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+
+        sut.display(feedWithFailedImageLoading())
+
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+    }
+    
+    // MARK: - Helpers
+
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func emptyFeed() -> [FeedImageCellController] {
+        return []
+    }
+    
+    private func feedWithContent() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil
+            )
+        ]
+    }
+    
+   
+    
+}
+
+
+private extension ListViewController {
+    func display(_ stubs: [ImageStub]) {
+        let cells: [FeedImageCellController] = stubs.map { stub in
+            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub)
+            stub.controller = cellController
+            return cellController
+        }
+        
+        display(cells)
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    let viewModel: FeedImageViewModel
+    let image: UIImage?
+    weak var controller: FeedImageCellController?
+
+    init(description: String?, location: String?, image: UIImage?) {
+        self.viewModel = FeedImageViewModel(
+            description: description,
+            location: location)
+        self.image = image
+    }
+    
+    func didRequestImage() {
+        controller?.display(ResourceLoadingViewModel(isLoading: false))
+        
+        if let image = image {
+            controller?.display(image)
+            controller?.display(ResourceErrorViewModel(message: .none))
+        } else {
+            controller?.display(ResourceErrorViewModel(message: "any"))
+        }
+    }
+    
+    func didCancelImageRequest() {}
+}
---
+++ CI/EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -0,0 +1,47 @@
+//
+//  ListSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Santiago Ochoa Bernaldo de Quiros on 28/8/24.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ListSnapshotTests: XCTestCase {
+    
+    func test_emptyList() {
+        let sut = makeSUT()
+        
+        sut.display(emptyFeed())
+
+        // We look for the same folder where the test file is located, and 'snapshots' folder images are located
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_dark")
+    }
+    
+    func test_listWithErrorMessage() {
+        let sut = makeSUT()
+
+        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+    }
+    
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        return controller
+    }
+    
+    private func emptyFeed() -> [CellController] {
+        return []
+    }
+    
+}
