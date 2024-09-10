e1d9216cb211c92b44f519860da297cfc67240b7
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-09-10 15:44:45 +0200

    Feed SUT on FeedSnapshotTests

--- CI/EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ CI/EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -95,7 +95,7 @@ class FeedSnapshotTests: XCTestCase {
 private extension ListViewController {
     func display(_ stubs: [ImageStub]) {
         let cells: [CellController] = stubs.map { stub in
-            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub)
+            let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
             stub.controller = cellController
             // STAR: We pass a unique id on the test because we don't want to keep track of the changes for the Diffable Data Source
             return CellController(id: UUID(), cellController)
