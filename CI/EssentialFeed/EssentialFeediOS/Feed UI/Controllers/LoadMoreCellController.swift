05216af5b7e891ba71484b6d047f5e574e55f81c
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-09-11 18:46:08 +0200

    Revert "Calling the callback for loading more before reaching the bottom of the tableView"
    This reverts commit c7a23b2fe4fb202eb079c07088935af665d276a8.

--- CI/EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ CI/EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -12,8 +12,6 @@ import EssentialFeed
 public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableViewDelegate {
     private let cell = LoadMoreCell()
     private let callback: () -> Void
-    private var offsetObserver: NSKeyValueObservation?
-
     
     public init(callback: @escaping () -> Void) {
         self.callback = callback
@@ -25,19 +23,12 @@ public class LoadMoreCellController: NSObject, UITableViewDataSource, UITableVie
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         cell.selectionStyle = .none
+
         return cell
     }
     
     public func tableView(_ tableView: UITableView, willDisplay: UITableViewCell, forRowAt indexPath: IndexPath) {
-        offsetObserver = tableView.observe(\.contentOffset, options: .new) { [weak self] (tableView, _) in
-            guard tableView.isDragging else { return }
-            
-            self?.reloadIfNeeded()
-        }
-    }
-    
-    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        offsetObserver = nil
+        reloadIfNeeded()
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
