ae965fec719db720ba2430799c01367a2469ad5f
Santiago Bernaldo de Quirós <quirosdeveloper@gmail.com>
2024-09-18 15:12:59 +0200

    Remove unused async methods

--- CI/EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ CI/EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -15,72 +15,21 @@ public protocol FeedStore {
     func insert(_ feed: [LocalFeedImage], timestamp: Date) throws
     func retrieve() throws -> CachedFeed?
     
-    typealias DeletionResult = Result<Void, Error>
-    typealias DeletionCompletion = (DeletionResult) -> Void
-    
-    typealias InsertionResult = Result<Void, Error>
-    typealias InsertionCompletion = (InsertionResult) -> Void
-    
-    typealias RetrievalResult = Result<CachedFeed?, Error>
-    typealias RetrievalCompletion = (RetrievalResult) -> Void
-    
-    // STAR:
-    /// The client is the responsible to use the right thread type, Main Queue to use this data on a main thread, or on a background thread
-    
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropiate thread, if needed
-    @available(*, deprecated)
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropiate thread, if needed
-    @available(*, deprecated)
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
-    
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to dispatch to appropiate thread, if needed
-    @available(*, deprecated)
-    func retrieve(completion: @escaping RetrievalCompletion)
-}
-
-public extension FeedStore {
-    func deleteCachedFeed() throws {
-        let group = DispatchGroup()
-        group.enter()
-        var result: DeletionResult!
-        deleteCachedFeed {
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
-        let group = DispatchGroup()
-        group.enter()
-        var result: InsertionResult!
-        insert(feed, timestamp: timestamp) {
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    func retrieve() throws -> CachedFeed? {
-        let group = DispatchGroup()
-        group.enter()
-        var result: RetrievalResult!
-        retrieve {
-            result = $0
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {}
-    func retrieve(completion: @escaping RetrievalCompletion) {}
+//    // STAR:
+//    /// The client is the responsible to use the right thread type, Main Queue to use this data on a main thread, or on a background thread
+//    
+//    /// The completion handler can be invoked in any thread.
+//    /// Clients are responsible to dispatch to appropiate thread, if needed
+//    @available(*, deprecated)
+//    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+//    
+//    /// The completion handler can be invoked in any thread.
+//    /// Clients are responsible to dispatch to appropiate thread, if needed
+//    @available(*, deprecated)
+//    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+//    
+//    /// The completion handler can be invoked in any thread.
+//    /// Clients are responsible to dispatch to appropiate thread, if needed
+//    @available(*, deprecated)
+//    func retrieve(completion: @escaping RetrievalCompletion)
 }
